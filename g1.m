d933a77f47cdabe73078b1364ea0eabbf430a00c
thenameismiggy <miguelroyatienza@gmail.com>
2016-12-19 21:19:38 +0800

    Fixed bugs in inverse kinematic computations

--- g1.m
+++ g1.m
@@ -26,28 +26,28 @@ for count = 3:size(numeral,1)
 end
 
 %computing for the coordinates of the rod - printhead intersections
-a1[1] = x + printheadoffset;
-a1[2] = y;
-a1[3] = z + tooloffset;
-b1[1] = x + (printheadoffset * cosd(120));
-b1[2] = x + (printheadoffset * sind(120));
-b1[3] = z + tooloffset;
-c1[1] = x + (printheadoffset * cosd(240));
-c1[2] = x + (printheadoffset * sind(240));
-c1[3] = z + tooloffset;
+a1(1) = x + printheadoffset;
+a1(2) = y;
+a1(3) = z + tooloffset;
+b1(1) = x + (printheadoffset * cosd(120));
+b1(2) = x + (printheadoffset * sind(120));
+b1(3) = z + tooloffset;
+c1(1) = x + (printheadoffset * cosd(240));
+c1(2) = x + (printheadoffset * sind(240));
+c1(3) = z + tooloffset;
 
 %computing for the coordinates of the rod - carriage intersections
-a2[1] = columnoffset;
-a2[2] = 0;
-b2[1] = columnoffset * cosd(120);
-b2[2] = columnoffset * sind(120);
-c2[1] = columnoffset * cosd(240);
-c2[2] = columnoffset * sind(240);
+a2(1) = columnoffset;
+a2(2) = 0;
+b2(1) = columnoffset * cosd(120);
+b2(2) = columnoffset * sind(120);
+c2(1) = columnoffset * cosd(240);
+c2(2) = columnoffset * sind(240);
 
 %computing for the horizontal length of the rods
-aa = sqrt(((a2[1] - a1[1])^2) + ((a2[2] - a1[2])^2));
-ab = sqrt(((b2[1] - b1[1])^2) + ((b2[2] - b1[2])^2));
-ac = sqrt(((c2[1] - c1[1])^2) + ((c2[2] - c1[2])^2));
+aa = sqrt(((a2(1) - a1(1))^2) + ((a2(2) - a1(2))^2));
+ab = sqrt(((b2(1) - b1(1))^2) + ((b2(2) - b1(2))^2));
+ac = sqrt(((c2(1) - c1(1))^2) + ((c2(2) - c1(2))^2));
 
 %computing for the vertical length of the rods
 ha = sqrt(abs((rodlength^2) - (aa^2)));
@@ -55,9 +55,9 @@ hb = sqrt(abs((rodlength^2) - (ab^2)));
 hc = sqrt(abs((rodlength^2) - (ac^2)));
 
 %computation for final carriage lengths
-out[1] = z + tooloffset + ha;
-out[2] = z + tooloffset + hb;
-out[3] = z + tooloffset + hc;
+out(1) = z + tooloffset + ha;
+out(2) = z + tooloffset + hb;
+out(3) = z + tooloffset + hc;
 
 end
 
