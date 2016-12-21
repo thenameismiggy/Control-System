c94602fa0b233860987632c4e961e095aeacaab7
thenameismiggy <miguelroyatienza@gmail.com>
2016-12-21 20:10:16 +0800

    Added in simple todo list

--- main.m
+++ main.m
@@ -18,7 +18,9 @@ while (~feof(fid))
             numeral = sscanf(currentline, '%c%d %c%d %c%d');
             switch(numeral(2))
                 case 82    %set extruder to absolute positioning
+				    %TODO
                 case 83    %set extruder to relative positioning
+				    %TODO
                 case 104    %set extruder temperature
                 case 107    %turn off fan
                 case 109    %set extruder temperature and wait
@@ -29,7 +31,9 @@ while (~feof(fid))
                 case 1    %move
                     data = g1(numeral);
                 case 2    %CW controlled arc move
+				    %TODO
                 case 3    %CCW controlled arc move
+				    %TODO
                 case 20    %set units to inches
                     rodlength = 8.99;
                     printheadoffset = 2.25;
@@ -41,9 +45,13 @@ while (~feof(fid))
                     columnoffset = 14.2748;
 					tooloffset = 7.5;
                 case 28    %move to origin
+					%TODO: function that sets printhead to origin if not already
                 case 90    %set to absolute positioning
+				    %TODO
                 case 91    %set to relative positioning
+				    %TODO
                 case 92    %set position
+				    %TODO: function that changes the position of origin
             end
         end	
     end
